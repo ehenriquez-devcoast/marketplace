43fa2575bee82008ee613002458b0c35ade4d074
StevenJ94 <stevenruiz.p94@gmail.com>
2021-11-07 10:49:06 -0500

    feat: add, por el momento se creó las rutas que tedrá el proyecto se podrían llegar añadir, quitar o cambiar de nombres en el fúturo También se crearon los componentes base de la vista inicial del dashboard A parttir de ahora se empezará a desarrollar

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -1,10 +1,70 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LandDetailsComponent } from './routes/land-details/land-details.component';
+import { ItemDetailsComponent } from './routes/item-details/item-details.component';
+import { BundleDetailsComponent } from './routes/bundle-details/bundle-details.component';
+
+import { LandComponent } from './routes/land/land.component';
+import { ItemComponent } from './routes/item/item.component';
+import { ProfileComponent } from './routes/profile/profile.component';
+import { BundleComponent } from './routes/bundle/bundle.component';
+import { DetailsComponent } from './routes/details/details.component';
+import { MarketplaceComponent } from './routes/marketplace/marketplace.component';
+import { DashboardComponent } from './routes/dashboard/dashboard.component';
+import { IndexComponent } from './routes/index/index.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+      },
+      {
+        path: 'marketplace',
+        component: MarketplaceComponent,
+      },
+      {
+        path: 'marketplace/:id',
+        component: DetailsComponent,
+      },
+      {
+        path: 'bundle',
+        component: BundleComponent,
+      },
+      {
+        path: 'bundle/:id',
+        component: BundleDetailsComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
+      {
+        path: 'item',
+        component: ItemComponent,
+      },
+      {
+        path: 'item/:id',
+        component: ItemDetailsComponent,
+      },
+      {
+        path: 'land',
+        component: LandComponent,
+      },
+      {
+        path: 'land/:id',
+        component: LandDetailsComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class WebsiteRoutingModule { }
+export class WebsiteRoutingModule {}
